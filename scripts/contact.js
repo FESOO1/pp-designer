a111c7b532a28ed0f47a4e4a79214993d5771b1b
FES001 <firdavs7794948@gmail.com>
2024-09-13 03:21:09 +0500

    Functionality is partially done.

--- scripts/contact.js
+++ scripts/contact.js
@@ -24,13 +24,67 @@ function copyEmail() {
 
 sendMessageBtn.addEventListener('click', e => {
     e.preventDefault();
-    sendMessageBtn.textContent = 'Sending...'
-    setTimeout(() => {
-        sendMessageBtn.textContent = 'Thank you';
-        sendMessageBtn.style.backgroundColor = 'RGB(50, 150, 50)';
-    }, 2000);
-    setTimeout(() => {
-        sendMessageBtn.textContent = 'Send Message';
-        sendMessageBtn.style.backgroundColor = '#303030';
-    }, 5000);
+
+    let errorContainer = [];
+
+    if (nameInput.value.length < 5) {
+        errorContainer.push('Less input entered');
+        nameInput.style.border = '1px solid red';
+    } else {
+        nameInput.style.border = '1px solid rgba(255,255,255,0.05)';
+    };
+    if (messageInput.value.length < 5) {
+        errorContainer.push('Less input entered');
+        messageInput.style.border = '1px solid red';
+    } else {
+        messageInput.style.border = '1px solid rgba(255,255,255,0.05)';
+    };
+    if (emailInput.value.length < 5) {
+        errorContainer.push('Less input entered');
+        emailInput.style.border = '1px solid red';
+    } else {
+        emailInput.style.border = '1px solid rgba(255,255,255,0.05)';
+    };
+
+
+    if (errorContainer.length === 0) {
+        sendMessageBtn.textContent = 'Sending...'
+        setTimeout(() => {
+            nameInput.value = '';
+            emailInput.value = '';
+            messageInput.value = '';
+            sendMessageBtn.textContent = 'Thank you';
+            sendMessageBtn.style.backgroundColor = 'RGB(50, 150, 50)';
+        }, 2000);
+        setTimeout(() => {
+            sendMessageBtn.textContent = 'Send Message';
+            sendMessageBtn.style.backgroundColor = '#303030';
+        }, 5000);
+    };
+});
+
+subscribeBtn.addEventListener('click', e => {
+    e.preventDefault();
+
+    let errorContainer = [];
+
+    if (subcribeEmailInput.value.length < 5) {
+        errorContainer.push('Less input entered');
+        subcribeEmailInput.style.border = '1px solid red';
+    } else {
+        subcribeEmailInput.style.border = '1px solid rgba(255,255,255,0.05)';
+    };
+
+    if (errorContainer.length === 0) {
+        subscribeBtn.textContent = 'Sending...'
+        setTimeout(() => {
+            subcribeEmailInput.value = '';
+            subscribeBtn.textContent = 'Thank you';
+            subscribeBtn.style.backgroundColor = 'RGB(50, 150, 50)';
+        }, 2000);
+        setTimeout(() => {
+            subscribeBtn.textContent = 'Send Message';
+            subscribeBtn.style.backgroundColor = '#303030';
+        }, 5000);
+    };
 });
